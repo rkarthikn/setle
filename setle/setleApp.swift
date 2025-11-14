942574411f9da2a9f0808a9b2099cda5576704af
rkarthikn <karthikn.raveendiran@gmail.com>
2025-11-14 19:57:42 +0530

    Initial Commit

---
+++ setle/setleApp.swift
@@ -0,0 +1,17 @@
+//
+//  setleApp.swift
+//  setle
+//
+//  Created by karthikeyan raveendiran on 14/11/25.
+//
+
+import SwiftUI
+
+@main
+struct setleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
